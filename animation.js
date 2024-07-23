727995759b97ffc7ea926695f5ef2c1f4adca69b
merveillelesline <merveillelesline6@gmail.com>
2024-07-23 15:02:29 +0100

    commith the status

---
+++ animation.js
@@ -0,0 +1,7 @@
+// Add your JavaScript code here
+// You can use JavaScript to handle interactivity and functionality
+
+// Example: Display a welcome message when the page loads
+window.addEventListener('load', function() {
+    alert('Welcome to REMGY!');
+  });
